e0c9a4f832f0eec3f36d094b7144ce85c631ef4f
Matt Cook <matt@zebradog.com>
2015-10-14 12:02:06 -0500

    updated calendar page formatting

--- themes/custom/westminster/js/schedule.js
+++ themes/custom/westminster/js/schedule.js
@@ -1,7 +1,19 @@
 jQuery(function(){
   var $ = jQuery;
   console.log("Schedule page loaded");
-  $('#schedule').fullCalendar({
+  $('#calendar').fullCalendar({
     // put your options and callbacks here
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    buttonText: {
+      today: 'today',
+      month: 'month',
+      week: 'week',
+      day: 'day'
+    },
+    editable: true
   });
 });
