cd9888071651a7cdb9101be31621c94462c67871
Nicolas Ross <nicolas@zebradog.com>
2017-12-07 16:26:36 -0600

    Added westminster_debug module, created /westminster/debug/routing form which manually rebuilds routes

---
+++ modules/westminster/westminster_debug/src/Form/RoutingForm.php
@@ -0,0 +1,39 @@
+<?php
+
+  namespace Drupal\westminster_debug\Form;
+
+  use Drupal\Core\Form\ConfigFormBase;
+  use Drupal\Core\Form\FormStateInterface;
+
+  Class RoutingForm extends ConfigFormBase {
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+      $form['actions']['submit'] = array(
+        '#type' => 'submit',
+        '#value' => $this->t('Rebuild Routes'),
+      );
+
+      return $form;
+    }
+
+    protected function getEditableConfigNames() {
+      return [
+        ConfigurationHelper::CONFIGURATION_NAME,
+      ];
+    }
+
+    public function getFormId() {
+      return 'westminster_debug_routing';
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+      \Drupal::service("router.builder")->rebuild();
+
+      drupal_set_message($this->t('Routes have been successfully rebuilt.'));
+    }
+
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    }
+
+  }
