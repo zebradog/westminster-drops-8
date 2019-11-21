3e34640f5329b3f75998ff5596d7f48152da9840
Nicolas Ross <nicolas@zebradog.com>
2019-11-21 16:58:24 -0600

    Add configuration to ContactForm

--- modules/westminster/westminster_support/src/Form/ContactForm.php
+++ modules/westminster/westminster_support/src/Form/ContactForm.php
@@ -5,12 +5,29 @@
   use \Drupal\Core\Form\FormBase;
   use \Drupal\Core\Form\FormStateInterface;
 
+  /**
+   * Contact form that appears on the main page.
+   */
   Class ContactForm extends FormBase {
+
+    /**
+     * Name of the module configuration object.
+     */
+    const CONFIGURATION_NAME = 'westminster_support.configuration';
+
+    /**
+     * Instance of the module configuration object.
+     * @see _getConfiguration()
+     * @var Config
+     */
+    protected $_configuration;
 
     /**
      * {@inheritdoc}
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
+      $configuration = $this->_getConfiguration();
+
       $form['subject'] = [
         '#title' => $this->t('Subject'),
         '#type' => 'textfield',
@@ -37,12 +54,26 @@
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
+      $configuration = $this->_getConfiguration();
+
       // TODO: Send email
       // TODO: Send notification
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state) {
       // TODO: Check required fields
+    }
+
+    /**
+     * Returns the module configuration object.
+     * @return Config
+     */
+    protected function _getConfiguration() {
+      if (!$this->_configuration) {
+        $this->_configuration = $this->config(static::CONFIGURATION_NAME);
+      }
+
+      return $this->_configuration;
     }
 
   }
